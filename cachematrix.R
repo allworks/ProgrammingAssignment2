76e12f54dd134e505fdd85a38611d1dc2d1802fc
allworks <cjjwang@hotmail.com>
2014-11-17 14:25:22 -0600

    Passed unit tests

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains two functions: makeCacheMatrix and cacheSolve
 
-## Write a short comment describing this function
+## makeCacheMatrix 
+## This function creates a special "matrix" object that can cache its inverse.
 
+## cacheSolve
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated and has not
+## changed, then the inverse is returned from the cache.
+
+## makeCacheMatrix
+## input: a matrix
+## return: a special "matrix object that can cache its inverse while providing
+##         four functions: getData, setData, setInverse, and getInverse
 makeCacheMatrix <- function(x = matrix()) {
+    
+    # private fields
+    this.inverse <- NULL
+    
+    # public methods
+    setData <- function(data) {
+        x <<- data   
+        this.inverse <<- NULL
+    }
+    
+    getData <- function() x
+    
+    setInverse <- function(inverse) {
+        this.inverse <<- inverse
+    }
+    
+    getInverse <- function() { this.inverse }
 
+    # return a list of public methods
+    list(set = setData, 
+         get = getData, 
+         setInverse= setInverse, 
+         getInverse= getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve
+## input: "matrix" object returned from makeCacheMatrix
+## return: the inverse of the matrix stored in the "matrix" object
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getInverse()
+    if (!is.null(inverse)) {
+        message("getting cached data")
+        return (inverse)
+    }
+    data <- x$get()
+    inverse <- solve(data)
+    
+    # cache the inverse
+    x$setInverse(inverse)
+    inverse
 }
